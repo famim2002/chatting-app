6445114cd50e062b201d7ee716dc92a1f94b6bd9
famim2002 <famimhayatyash@gmail.com>
2025-05-15 11:49:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes  path="/" elements="">
+          <Route  />
+        </Routes>
+      </BrowserRouter> 
+    </>
+  )
+}
+
+export default App
